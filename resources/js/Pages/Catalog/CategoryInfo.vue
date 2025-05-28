b984223793f9a4c0b75465ab48c0bdeab1a9563d
Aleksandr <alik.muradyan92@gmail.com>
2025-05-29 02:02:36 +0400

    fix

--- resources/js/Pages/Catalog/CategoryInfo.vue
+++ resources/js/Pages/Catalog/CategoryInfo.vue
@@ -14,12 +14,12 @@
     </div>
     <div class="category_info_content_middle">
       <div class="content_middle_left">
-        <div class="title" v-if="categoryInfoData.subsubcategory">{{categoryInfoData.subsubcategory}}</div>
-        <div class="title" v-else-if="categoryInfoData.subCategory">{{categoryInfoData.subCategory}}</div>
-        <div class="title" v-else>{{categoryInfoData.category}}</div>
-        <div class="text"  v-if="categoryInfoData.subsubcategoryDesc" v-html="categoryInfoData.subsubcategoryDesc"></div>
+        <div class="title" v-if="categoryInfoData.subsubcategory">{{categoryInfoData.subsubcategoryDesc}}</div>
+        <div class="title" v-else-if="categoryInfoData.subCategory">{{categoryInfoData.subCategoryDesc}}</div>
+        <div class="title" v-else>{{categoryInfoData.categoryDesc}}</div>
+        <!-- <div class="text"  v-if="categoryInfoData.subsubcategoryDesc" v-html="categoryInfoData.subsubcategoryDesc"></div>
         <div class="text" v-else-if="categoryInfoData.subCategoryDesc" v-html="categoryInfoData.subCategoryDesc"></div>
-        <div class="text" v-else v-html="categoryInfoData.categoryDesc"></div>
+        <div class="text" v-else v-html="categoryInfoData.categoryDesc"></div> -->
       </div>
       <div class="content_middle_right" v-if="categoryInfoData.category">
         <img :src="categoryInfoData.second_image">
