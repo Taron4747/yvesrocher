8aabcfa7b3d43988ef8fe7ddd63aa556771ae1f0
Aleksandr <alik.muradyan92@gmail.com>
2025-05-31 00:31:00 +0400

    fix

--- resources/js/Pages/Catalog/Index.vue
+++ resources/js/Pages/Catalog/Index.vue
@@ -69,8 +69,7 @@
             <a :href="/product/+item.id" class="product_image">
               <div class="img_wrapper">
                 <img :src="item.image" class="main_img">
-                <img :src="item.images[0].path" class="hover_img" v-if="item.images[0].path">
-                <img :src="item.image" class="hover_img" v-esle>
+                <img :src="item.images[0].path" class="hover_img">
               </div>
               <div class="product_type" v-if="item.is_new == 1">{{ this.$page.props.language.new }}</div>
               <div class="product_type" v-if="item.is_bestseller == 1">{{ this.$page.props.language.Bestsellers }}</div>
